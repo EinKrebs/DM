f2bb19b3a1b9860ffbbe20cac5bbf0160054c35c
Yury Frolov <yura200253@gmail.com>
2020-11-23 09:42:07 +0500

    A

---
+++ cmake-build-debug/CMakeFiles/DM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DM.dir/main.cpp.o"
+  "DM"
+  "DM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/DM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DM.
+# This may be replaced when dependencies are built.
